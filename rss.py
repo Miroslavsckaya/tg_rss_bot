1dd2ccfc6eda4b2251049c7e1132f81198799432
mitsuha_s <stmiroslavsckaya@yandex.ru>
2022-05-29 20:25:07 +0000

    add get_feed method

--- rss.py
+++ rss.py
@@ -5,8 +5,7 @@ class FeedItem():
     pass
 
 class Feed():
-    def __init__(self, title: str, url: str, items: list) -> None:
-        self.title = title
+    def __init__(self, url: str, items: list) -> None:
         self.url = url
         self.items = items
 
@@ -18,6 +17,6 @@ class RssReader():
         pass
 
     def get_feed(self, url: str) -> Feed:
-        pass
-
-
+        f = feedparser.parse(url)
+        items = self.get_items(f.entries)
+        return Feed(url, items)
